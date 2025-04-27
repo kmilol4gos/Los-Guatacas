6732c86aaaa51b6969adc2b8d00f920e1fd2c185
Camilo Lagos I <67244497+kmilol4gos@users.noreply.github.com>
2025-04-27 01:22:13 -0400

    Enhance Hero and Promotion components with improved styles; adjust opacity in Layout

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,10 +8,10 @@ export function Hero() {
         <>
             <main class="text-gray-900 flex flex-col gap-10 items-center" id="inicio">
                 <header class="flex flex-col gap-5 items-center">
-                    <h1 class="text-4xl font-bold text-yellow-guataca uppercase text-center">
+                    <h1 class="text-5xl font-bold text-yellow-guataca uppercase text-center">
                         {localName}
                     </h1>
-                    <h2 class="text-2xl font-semibold text-neutral-50 text-center">
+                    <h2 class="text-2xl font-semibold text-neutral-50 text-center drop-shadow-lg">
                         {localDescription}
                     </h2>
                 </header>
